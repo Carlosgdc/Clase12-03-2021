64f0cf3d2edd2bb78cd087ebd15c30beabb17d0a
Carlosgdc <cdiazca@unal.edu.co>
2021-04-06 18:32:40 -0500

    En este programa se observa como se manipulan los bits directamente para obtener resultados en terminos de optimización. NOTA: Es muy arriesgado realizar este tipo de operaciones.

--- Clase12-03-2021/main.cpp
+++ Clase12-03-2021/main.cpp
@@ -4,35 +4,20 @@
 //
 //  Created by Countyfoot on 4/3/21.
 //
-// Este programa nos muestra como usar algunos operadores compuestos.
-// Use a debugger to visualize
+//En este programa se observa como se manipulan los bits directamente para obtener resultados en terminos de optimización. NOTA: Es muy arriesgado realizar este tipo de operaciones.
 #include <iostream>
 #include <cstdlib>
 #include <cmath>
 
 int main (int argc, char *argv[])
 {
-  double  a = 0, b = 3.1;
-  
-  a = a + 7;
- 
-  a += 7; // a = a + 7
-  b *= 2; // b = b*2
-  a += 2*b; // a = a + 2*b;
+  int a = 6; // 110
+  int b = 4; // 100
 
-  int m = 4, n = 4;
-  m /= 2; // m = m/2
-
-  n &= 1; // n = n&1// i logico a nivel de bits
-  // n : 100
-  // 1 : 001
-  //n&1: 000
-
-  n = 4;
-  n |= 1; // n = n|1 // o logico a nivel de bits
-  // n : 100
-  // 1 : 001
-  //n|1: 101
-  
+  int c = a & b; // c = 100 = 4
+  c = a | b; // c = 110 = 6
+  c = a ^ b; // c = 010 = 2
+  c = ~a;    // c = 001 = -7
+  c = a<<2;  // c = 11000 =
   return 0;
 }
